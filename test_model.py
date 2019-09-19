e749ecbd5d222b364ca546a93efc2daa3edac955
Lam Nguyen <lam.nguyen.hy.hoai@gmail.com>
2019-09-19 18:42:05 +0700

    Changed format output
    Changed output format
    
    test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    test
    
    Updated result files' extension
    
    Updated result files' extension
    
    test
    
    test
    
    test
    
    test

--- test_model.py
+++ test_model.py
@@ -1,12 +1,16 @@
 # python test_model.py model=iphone_orig dped_dir=dped/ test_subset=full iteration=all resolution=orig use_gpu=true
 
 from scipy import misc
+from PIL import Image
+from models import resnet
+from tqdm import tqdm
+
 import numpy as np
 import tensorflow as tf
-from models import resnet
 import utils
 import os
 import sys
+import matplotlib.pyplot as plt
 
 # process command arguments
 phone, dped_dir, test_subset, iteration, resolution, use_gpu = utils.process_test_model_args(sys.argv)
@@ -42,28 +46,33 @@ with tf.Session(config=config) as sess:
         saver = tf.train.Saver()
         saver.restore(sess, "models_orig/" + phone)
 
-        for photo in test_photos:
+        for photo in tqdm(test_photos):
 
             # load training image and crop it if necessary
 
-            print("Testing original " + phone.replace("_orig", "") + " model, processing image " + photo)
-            image = np.float16(misc.imresize(misc.imread(test_dir + photo), res_sizes[phone])) / 255
+            # print("Testing original " + phone.replace("_orig", "") + " model, processing image " + photo)
+            image = np.float16(misc.imresize(misc.imread(test_dir + photo, mode="RGB"), res_sizes[phone])) / 255
 
             image_crop = utils.extract_crop(image, resolution, phone, res_sizes)
             image_crop_2d = np.reshape(image_crop, [1, IMAGE_SIZE])
 
             # get enhanced image
 
-            enhanced_2d = sess.run(enhanced, feed_dict={x_: image_crop_2d})
-            enhanced_image = np.reshape(enhanced_2d, [IMAGE_HEIGHT, IMAGE_WIDTH, 3])
+            enhanced_2d = np.array(sess.run(enhanced, feed_dict={x_: image_crop_2d})[0]) * 255
+            enhanced_2d = np.clip(enhanced_2d, 0, 255)
+            enhanced_2d = enhanced_2d.astype("uint8")
 
-            before_after = np.hstack((image_crop, enhanced_image))
-            photo_name = photo.rsplit(".", 1)[0]
+            original_image = Image.open(test_dir + photo).resize((256, 256))
+            enhanced_image = Image.fromarray(enhanced_2d).resize((256, 256))
+
+            # before_after = np.hstack((image_crop, enhanced_image))
+            # photo_name = photo.rsplit(".", 1)[0]
 
             # save the results as .png images
 
-            misc.imsave("visual_results/" + phone + "_" + photo_name + "_enhanced.png", enhanced_image)
-            misc.imsave("visual_results/" + phone + "_" + photo_name + "_before_after.png", before_after)
+            plt.imsave("results/" + photo, np.array(enhanced_image))
+            # misc.imsave("visual_results/" + phone + "_" + photo_name + "_enhanced.png", enhanced_image)
+            # misc.imsave("visual_results/" + phone + "_" + photo_name + "_before_after.png", before_after)
 
     else:
 
